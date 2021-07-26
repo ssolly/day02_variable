6d9ecdc67495235e986427f41a64cb4fbe84b791
Choi.SY <sollly0829@gmail.com>
2021-07-26 10:43:49 +0900

    Ex02 변수 관련 기본 예제

--- src/day02_variable/Ex01.java
+++ src/day02_variable/Ex01.java
@@ -1,5 +1,36 @@
 package day02_variable;
 
 public class Ex01 {
-
+	public static void main(String[] args) {
+		
+		/*
+		 # 변수 작성 방법
+		  - 자료형 변수이름;
+		  - 자료형 변수이름 = 값;
+		 # 변수 
+		  : 무언가(data)를 저장하기 위한 공간
+		 # 변수명 작성 규칙
+		  - 될 수 있으면 영어로 작성한다
+		  - 변수명의 첫 글자는 소문자로 작성한다
+		  - 만약 합성어가 존재한다면 합성어의 첫 글자는 대문자로 짓는다
+		 */
+		
+		/*
+		 # 자료형
+		  1) 정수
+		    - short : 2byte
+		    - int : 4byte
+		  2) 실수
+		    - float : 4byte
+		    - double : 8byte
+		  3) 문자
+		    - byte : 1byte
+		    - char : 2 byte
+		  4) 논리형
+		    - boolean : 1byte
+		                true(참), false(거짓)만 표현 가능
+		 # 컴퓨터 단위
+		  - bit < byte < KByte < MByte < GByte < TByte
+		 */
+	}
 }
